a5859fc20cd67393f7a6d226df4a6375a081f627
James A Abolt <jaboltj@jci.com>
2018-06-16 22:16:44 -0500

    Not a clue

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace zymur1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
